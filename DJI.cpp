4501b8b9c49ef909a1d7cb9595d4474d287b8714
MarcoZhu03 <zhuyongchao94@163.com>
2018-07-07 21:14:06 +0800

    DJI

---
+++ DJI.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, _TCHAR* argv[])
+{
+	string str;
+	cin>>str;
+	int num=1;
+	char judge=str[0];
+	string::iterator iter=str.begin();
+	if(str.size()==0)
+	{
+		throw "an error occured";
+	}
+	else
+	{
+		iter++;
+		while(iter!=str.end())
+		{
+			
+			if(*iter==judge){
+				num++;
+			} 
+			else{
+				cout<<judge<<num;
+				num=1;
+				judge=*iter;
+			}
+			iter++;
+		}
+	}
+	cout<<judge<<num;
+	system("pause");
+	return 0;
+}
