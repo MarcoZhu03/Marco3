df3d7c3eb827191fe603524605e8b11ba6fc5b5f
MarcoZhu03 <zhuyongchao94@163.com>
2018-07-07 16:14:21 +0800

    add a case

---
+++ RotateArray.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    int minNumberInRotateArray(vector<int> rotateArray) {
+    if(rotateArray.size()==0)
+        return 0;
+    else
+    {
+        for(int num=0; num<rotateArray.size(); num++)
+        {
+            if(rotateArray[num]>rotateArray[num+1])
+                return rotateArray[num+1];
+        }
+    }
+    return rotateArray[0];
+    }
+};
