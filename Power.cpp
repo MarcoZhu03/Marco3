dfec5628faae87723fe5b6a52dff3c700181fdf7
MarcoZhu03 <zhuyongchao94@163.com>
2018-07-07 17:01:02 +0800

    power

---
+++ Power.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+    double Power(double base, int exponent) {
+    double temp=1;
+    if(exponent>=0)
+    {
+        for(int num=0; num<exponent; num++)
+        {
+            temp=temp*base;
+        }
+    }
+    else
+    {
+        for(int num=0; num<-exponent; num++)
+        {
+            temp=temp*base;
+        }
+        temp=1/temp;
+    }
+    return temp;
+    }
+};
