5219984b2199d944999fad92d44ad6c0f0b6bd65
MarcoZhu03 <zhuyongchao94@163.com>
2018-07-07 15:57:25 +0800

    Marco's first commit

---
+++ FindNumber.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array ) {
+        int length=array.size();
+        int lengthCo=array[0].size();
+        if(length==0||lengthCo==0)
+        {
+            return false;
+        }
+        else
+        {
+            for(int i=0; i<length; i++)
+            {
+                for(int j=0; j<lengthCo; j++)
+                {
+                    if(array[i][j]==target)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        ;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+};
---
+++ ReplaceSpace.cpp
@@ -0,0 +1,13 @@
+﻿class Solution {
+public:
+	void replaceSpace(char *str,int length) {
+    	string str2(str);
+    	int i=0;
+    	while((i=str2.find(' '))!=string::npos){
+        str2.erase(i,1);
+        str2.insert(i,"%20");
+	//str2.replace(i,1,"%20");//Another method
+    	}      
+    	strcpy(str,str2.c_str());//string to char*
+	}
+};
